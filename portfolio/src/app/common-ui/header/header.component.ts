d5900d979f315e25179ed1d0cb71b4dc4c74f253
Kapil-Ingle <codingkapil@gmail.com>
2025-11-30 23:16:50 +0530

    About screen added and CSS changes.

--- portfolio/src/app/common-ui/header/header.component.ts
+++ portfolio/src/app/common-ui/header/header.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
 
+export class HeaderComponent {
+  private router = inject(Router);
+
+  navigateTo(routerLink: string){
+    this.router.navigate([routerLink])
+  }
+  
 }
