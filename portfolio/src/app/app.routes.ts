9f27cf1ea9c36c3444ae70854422ad421b505ec7
Kapil-Ingle <codingkapil@gmail.com>
2025-12-04 21:00:41 +0530

    Skill component code.

--- portfolio/src/app/app.routes.ts
+++ portfolio/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
+import { SkillsComponent } from './pages/skills/skills.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent},
     {path: 'about', component: AboutComponent},
+    {path: 'skills', component: SkillsComponent},
 ];
